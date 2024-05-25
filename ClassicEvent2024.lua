839a9339f1a5a62e460bff99c6ba0352d5f69346
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-05-25 07:38:28 +0700

    Update ClassicEvent2024.lua

--- ClassicEvent2024.lua
+++ ClassicEvent2024.lua
@@ -13,43 +13,43 @@ local Window = Library:CreateWindow({
 })
 local Tab = Window:AddTab("Main")
 local Section = Tab:AddLeftGroupbox("Select a game")
-Section:AddButton({Text="Toilet Tower Defense",Default=false,Func=function()
+Section:AddButton({Text="Toilet Tower Defense",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicToiletTower?raw=true"))()
     end})
-Section:AddButton({Text="Restaurant",Default=false,Func=function()
+Section:AddButton({Text="Restaurant",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicRestaurant?raw=true"))()
     end})
-Section:AddButton({Text="Livetopia",Default=false,Func=function()
+Section:AddButton({Text="Livetopia",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicLivetopia?raw=true"))()
     end})
-Section:AddButton({Text="Gunfight",Default=false,Func=function()
+Section:AddButton({Text="Gunfight",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicGunfight?raw=true"))()
     end})
-Section:AddButton({Text="Driving Empire",Default=false,Func=function()
+Section:AddButton({Text="Driving Empire",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicDrivingEmpire?raw=true"))()
     end})
-Section:AddButton({Text="Dress Tolmpress",Default=false,Func=function()
+Section:AddButton({Text="Dress Tolmpress",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicDressToImpress?raw=true"))()
     end})
-Section:AddButton({Text="Clip It",Default=false,Func=function()
+Section:AddButton({Text="Clip It",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicClipIt?raw=true"))()
     end})
-Section:AddButton({Text="Blade Ball",Default=false,Func=function()
+Section:AddButton({Text="Blade Ball",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicBladeBall?raw=true"))()
     end})
-Section:AddButton({Text="Bee Swarm",Default=false,Func=function()
+Section:AddButton({Text="Bee Swarm",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicBeeSwarm?raw=true"))()
     end})
-Section:AddButton({Text="Bed Wars",Default=false,Func=function()
+Section:AddButton({Text="Bed Wars",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicBedWars?raw=true"))()
     end})
-Section:AddButton({Text="Arsenal",Default=false,Func=function()
+Section:AddButton({Text="Arsenal",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicArsenal?raw=true"))()
     end})
-Section:AddButton({Text="A Dusty Trip",Default=false,Func=function()
+Section:AddButton({Text="A Dusty Trip",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicAdustyTrip?raw=true"))()
     end})
-Section:AddButton({Text="Adopt Me",Default=false,Func=function()
+Section:AddButton({Text="Adopt Me",Default=false,DoubleClick=true,Func=function()
       loadstring(game:HttpGet("https://github.com/ToraScript/Script/blob/main/ClassicAdoptMe?raw=true"))()
     end})
 Section:AddLabel("All Script Made By : ToraIsMe")
@@ -61,8 +61,8 @@ game:GetService("RunService").RenderStepped:Connect(function()
 	game.Players.LocalPlayer.Character.Humanoid.JumpPower = 75
     end)
 end})
-Section2:AddToggle("MyToggle",{Text="No Darkness Effect",Default=false,Callback=function(1)
-            if 1 then
+Section2:AddToggle("MyToggle",{Text="No Darkness Effect",Default=false,Callback=function(v1)
+            if v1 then
                 game.Lighting.Brightness = 2
                 game.Lighting.FogEnd = 9e9
                 game.Lighting.OutdoorAmbient = Color3.new(1,1,1)
@@ -72,3 +72,20 @@ Section2:AddToggle("MyToggle",{Text="No Darkness Effect",Default=false,Callback=
                 game.Lighting.OutdoorAmbient = Color3.new(0,0,0)
             end
         end})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
